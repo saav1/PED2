9f55fe0b30ef59a20f8f98fbeab543ba6441775a
= <=>
2019-04-09 17:17:08 +0200

    tabbporo.cpp

--- lib/tabbporo.cpp
+++ lib/tabbporo.cpp
@@ -1,2 +1,35 @@
 #include "tabbporo.h"
-//.....................................................//
+//....................TNodoABB...............................//
+
+//Constructor
+TNodoABB::TNodoABB():item(), iz(), de(){}
+
+//Constructor de copia
+TNodoABB::TNodoABB(const TNodoABB &nodoAbb){
+	if(this != nodoAbb){
+		this->item = nodoAbb.item;
+		this->iz = nodoAbb.iz;
+		this->de = nodoAbb.de;
+	}
+}
+
+//Destructor
+TNodoABB::~TNodoABB(){
+	this->item = TPoro();
+	this->iz = TABBPoro();
+	this->de = TABBPoro();
+}
+
+//Sobrecarga del operador de asignación
+TNodoABB & TNodoABB::operator = (const TNodoABB &nodoAbb){
+	if(this != nodoAbb){
+		this->item = nodoAbb.item;
+		this->iz = nodoAbb.iz;
+		this->de = nodoAbb.de;
+	}
+	return(*this);
+}
+//...........................................................//
+
+//....................TABBPoro...............................//
+//...........................................................//
